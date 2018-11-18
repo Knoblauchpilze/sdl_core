fb30b3db1963997cf7b5cce7486396e9388c7d8a
KnoblauchPC <prolific-programmer@tuta.com>
2018-11-18 11:37:43 +0100

    Modified the way we handle transparency in sdl widget

--- src/SdlWidget.cc
+++ src/SdlWidget.cc
@@ -7,15 +7,31 @@ namespace sdl {
     SdlWidget::SdlWidget(const std::string& name,
                          const Boxf& area,
                          SdlWidget* parent,
-                         const SDL_Color& color,
-                         const SDL_BlendMode& blendMode):
+                         const SDL_Color& color):
       SdlEventListener(SdlEventListener::Interaction::MouseButtonReleased),
 
       m_name(name),
       m_parent(parent),
       m_area(area),
       m_background(color),
-      m_blendMode(blendMode),
+
+      // This custom blend mode is mainly used to be able to have additive alpha blending in children widget.
+      // Basically if a widget has a transparency of 128 and one of its children has also a transparency of 128,
+      // we would want the final pixel to have a transparency of 64 (i.e. one fourth of totally opaque).
+      // This cannot be achieved using any of the proposed blend modes so we have to rely on custom blend mode.
+      // The custom blend mode can be composed as follows:
+      // dstRGB = colorOperation(srcRGB * srcColorFactor, dstRGB * dstColorFactor)
+      // dstA = alphaOperation(srcA * srcAlphaFactor, dstA * dstAlphaFactor)
+      m_blendMode(
+        SDL_ComposeCustomBlendMode(
+          SDL_BLENDFACTOR_SRC_ALPHA,           // srcColorFactor
+          SDL_BLENDFACTOR_ONE_MINUS_SRC_ALPHA, // dstColorFactor
+          SDL_BLENDOPERATION_ADD,              // colorOperation
+          SDL_BLENDFACTOR_DST_ALPHA,           // srcAlphaFactor
+          SDL_BLENDFACTOR_ONE_MINUS_SRC_ALPHA, // dstAlphaFactor
+          SDL_BLENDOPERATION_ADD               // alphaOperation
+        )
+      ),
 
       m_dirty(true),
       m_isDrawable(true),

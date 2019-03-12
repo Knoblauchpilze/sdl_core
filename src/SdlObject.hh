1bdbc1b6b15d2d457b4a202b3620ff7de270223a
KnoblauchPC <prolific-programmer@tuta.com>
2019-03-12 22:49:30 +0100

    Added first empty implementation of SdlObject, which will contain basis for logging and error handling

---
+++ src/SdlObject.hh
@@ -0,0 +1,19 @@
+#ifndef    SDL_OBJECT_HH
+# define   SDL_OBJECT_HH
+
+namespace sdl {
+  namespace core {
+
+    class SdlObject {
+      public:
+
+      protected:
+
+    };
+
+  }
+}
+
+# include "SdlObject.hxx"
+
+#endif    /* SDL_OBJECT_HH */
